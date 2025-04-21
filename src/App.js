47aecbf09dc320549a676d48033ad05e711c0cff
Edgar Santiago Ariza Garcia <edgarariza6@outlook.com>
2025-04-20 23:58:34 -0500

    Expandimos planeacion
    creacion de carpetas y archivos para la creacion del prototipo del frontend ademas de mejora la expansion del desarrollo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
+import './styles/App.css';
 import Layout from './components/layout';
 import Home from './pages/home';
 import Detail from './pages/detail';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+/* 
+import Cart from "./components/Cart"; // Ya está en components
+import NavBar from "./components/NavBar"; // Nuevo componente de navegación
+import Footer from "./components/Footer"; // Nuevo componente de pie de página 
+// */
+
 function App() {
   return (
     <BrowserRouter>
