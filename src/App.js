50116e472835b65a223843645537178853ef6a60
Edgar Santiago Ariza Garcia <edgarariza6@outlook.com>
2025-04-21 15:16:52 -0500

    Integracion de las funciones del frontend
    Se agregan las paginas principales, la tienda, el carrito de compras, la clasificacion, etc.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import './styles/App.css';
-import Layout from './components/layout';
-import Home from './pages/home';
-import Detail from './pages/detail';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-/* 
-import Cart from "./components/Cart"; // Ya está en components
-import NavBar from "./components/NavBar"; // Nuevo componente de navegación
-import Footer from "./components/Footer"; // Nuevo componente de pie de página 
-// */
+import React from "react";
+import Header from "./common/Header";
+import Footer from "./common/Footer";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import NotFoundPage from "./pages/NotFoundPage";
+import Shop from "./pages/Shop";
+import ContactUs from "./pages/Contact";
+import AboutUs from "./pages/AboutUs";
+import Blog from "./pages/Blog";
 
-function App() {
+const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path='/:slug' element={<Detail />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <Router>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/*" element={<NotFoundPage />} />
+        </Routes>
+        <Footer />
+      </Router>
+      {/* for suggestion we need to hit ctrl + space */}
+    </div>
   );
-}
+};
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      colors: {
-        primary: '#19535F',      // Azul oscuro
-        secondary: '#0B7A75',    // Verde azulado
-        accent: '#7B2D26',       // Rojo terroso
-        beige: '#D7C9AA',        // Color crema principal
-        light: '#F0F3F5',        // Fondo claro
-      }
-    },
+    extend: {},
   },
   plugins: [],
-}
-
+};
